08d962a7151bedace923f117f0929532d3915f79
hkpatel321 <phet30440@gmail.com>
2025-08-11 14:33:06 +0530

    Added Sidebar

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Sidebar from './Sidebar';
 import Header from './Header';
 
 const Layout = () => {
+  const { sidebarCollapsed } = useSelector((state) => state.ui);
+
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-gradient-to-br from-primary-50 via-secondary-50 to-accent-50 relative overflow-hidden">
+      {/* Animated background elements */}
+      <div className="absolute inset-0 opacity-30">
+        <div className="absolute top-0 -left-4 w-72 h-72 bg-primary-300 rounded-full mix-blend-multiply filter blur-xl animate-float"></div>
+        <div className="absolute top-0 -right-4 w-72 h-72 bg-secondary-300 rounded-full mix-blend-multiply filter blur-xl animate-float animation-delay-2000"></div>
+        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-accent-300 rounded-full mix-blend-multiply filter blur-xl animate-float animation-delay-4000"></div>
+      </div>
+      
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex-1 flex flex-col relative z-10">
         <Header />
-        <main className="h-[calc(100vh-4rem)] overflow-auto p-6">
+        <main className="flex-1 overflow-auto p-6">
           <Outlet />
         </main>
       </div>
